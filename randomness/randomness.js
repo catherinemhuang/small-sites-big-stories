b59990f9a72f457bc3265a567a89c4bc7eedcc65
catherinemhuang <chuang05@risd.edu>
2026-03-30 10:45:23 -0400

    added emojis

--- randomness/randomness.js
+++ randomness/randomness.js
@@ -1,1 +1,23 @@
 console.log("This is the randomness site!");
+let container = document.querySelector('.container');
+container.style.transform = `rotate(${Math.random()*90-45}deg) scale(${Math.random()*5+3})`;
+
+let randomWords = [
+	"😂",
+	"✌",
+	"🐻",
+	"🎂",
+	"🥑",
+	"😀",
+    "🏆",
+    "🥝",
+    "🌞",
+    "🎪"
+]
+let randomWord = randomWords[Math.floor(Math.random()*randomWords.length)];
+
+container.innerHTML = randomWord;
+if (Math.random() < .5) {
+	let randomWord = randomWords[Math.floor(Math.random()*randomWords.length)];
+	container.innerHTML += " " + randomWord;
+}
