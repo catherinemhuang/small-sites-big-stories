861880ea412a09203cd6b0fb31b61d6470a8c4ea
catherinemhuang <chuang05@risd.edu>
2026-03-30 19:33:26 -0400

    Update time.js

--- time/time.js
+++ time/time.js
@@ -55,8 +55,32 @@ setTimeout(() => {
 setTimeout(() => {
     container.innerText = "";
     let img = document.createElement("img");
-    img.src = "https://fbi.cults3d.com/uploaders/15008782/illustration-file/869010b7-0dd9-4f4d-b5ef-6202b311198e/deathstar.png"; 
+    img.src = "https://miro.medium.com/1*yalF4RzL5pesvSspDfNFpw.jpeg"; 
     img.style.width = "100%"; 
-    img.style.height = "108%";
+    img.style.height = "100%";
     container.appendChild(img);
 }, 23000);
+setTimeout(() => {
+	container.innerText = "now its all up to him";
+	container.style.fontSize = "50px";
+}, 25000)
+setTimeout(() => {
+    container.innerText = "";
+    let img = document.createElement("img");
+    img.src = "https://cdn.prod.website-files.com/65fda6ec250492511fc9e567/683ee5c36e346681b84fc5bd_AD_4nXdwt9yLr1YnVlaUTYcOUXhNRYJJaIav3YEqIPLRVxZXYvY0eDfo0MZAynTvUUaHK0W0oC5x7BSwb5rlPsuGTW-f5e8X-bU-ubMRhRMQYv8g3d2wZZz6tGuiCOu5j6IfzK1YpTiE.png"; 
+    img.style.width = "100%"; 
+    img.style.height = "100%";
+    container.appendChild(img);
+}, 26000);
+setTimeout(() => {
+	container.innerText = "and him";
+	container.style.fontSize = "50px";
+}, 27000)
+setTimeout(() => {
+    container.innerText = "";
+    let img = document.createElement("img");
+    img.src = "https://pyxis.nymag.com/v1/imgs/8ac/205/3c2b6e5b004cd06afe9a4f984768d0b8ba-23-han-solo.2x.h473.w710.jpg"; 
+    img.style.width = "90%"; 
+    img.style.height = "90%";
+    container.appendChild(img);
+}, 28000);
