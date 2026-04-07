0abcb4874a12160e7bba5b2b29556041730063e8
catherinemhuang <chuang05@risd.edu>
2026-04-07 09:47:49 -0400

    update 4/7

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -1,26 +1,15 @@
 console.log("This is the interaction site!");
 
-const block = document.querySelector(".block");
-
-let isDragging = false;
-let offsetX = 0;
-let offsetY = 0;
-
-block.addEventListener("mousedown", (e) => {
-  isDragging = true;
-
-  offsetX = e.clientX - block.offsetLeft;
-  offsetY = e.clientY - block.offsetTop;
-});
-
-document.addEventListener("mousemove", (e) => {
-  if (!isDragging) return;
-
-  block.style.position = "absolute";
-  block.style.left = `${e.clientX - offsetX}px`;
-  block.style.top = `${e.clientY - offsetY}px`;
-});
-
-document.addEventListener("mouseup", () => {
-  isDragging = false;
-});
+let block = document.querySelector('.block');
+let numberOfInteractions = 0;
+function runAway() {
+	block.style.transform = `translate(${Math.random()*100-50}vw, ${Math.random()*100-50}vh)`;
+	numberOfInteractions++;
+	if (numberOfInteractions > 10) {
+		block.innerText = "AHHHHHH";
+	}
+	if (numberOfInteractions > 20) {
+		block.innerText = "STOP IT STOP IT STOOOOOP";
+	}
+}
+block.addEventListener('mouseenter', runAway)
