5f7ac5d6c23527831e4e641e039bf97214e0ca2b
catherinemhuang <chuang05@risd.edu>
2026-04-08 11:29:29 -0400

    update

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -2,14 +2,15 @@ console.log("This is the interaction site!");
 
 let block = document.querySelector('.block');
 let numberOfInteractions = 0;
+
 function runAway() {
 	block.style.transform = `translate(${Math.random()*100-50}vw, ${Math.random()*100-50}vh)`;
 	numberOfInteractions++;
-	if (numberOfInteractions > 10) {
-		block.innerText = "AHHHHHH";
+	if (numberOfInteractions > 2) {
+		block.innerText = "hahahhaa";
 	}
-	if (numberOfInteractions > 20) {
-		block.innerText = "STOP IT STOP IT STOOOOOP";
+	if (numberOfInteractions > 4) {
+		block.innerText = "You can't get me";
 	}
 }
 block.addEventListener('mouseenter', runAway)
