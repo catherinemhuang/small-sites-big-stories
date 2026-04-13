ab49b35f81bf2084fdecb1d3b65a0b86df5caaf2
catherinemhuang <chuang05@risd.edu>
2026-04-13 19:08:10 -0400

    added files

--- loop/loop.js
+++ loop/loop.js
@@ -1,1 +1,45 @@
 console.log("This is the loop site!");
+		let container = document.querySelector('.container');
+
+		let i = 0;
+		while (i < 100) {
+
+			let randomSize = Math.random() * 100;
+
+			let r = Math.floor(Math.random() * 100);
+			let g = Math.floor(Math.random() * 100);
+			let b = Math.floor(Math.random() * 255);
+			let opacity = Math.random();
+
+			let newDiv = `
+				<div style="
+					width: ${randomSize}px;
+					height: ${randomSize}px;
+					background-color: rgba(${r}, ${g}, ${b}, ${opacity});
+				"></div>
+			`;
+
+			container.innerHTML += newDiv;
+			i++;
+		}
+
+		function changeSizes() {
+			let allChildren = document.querySelectorAll('.container div');
+
+			for (let i = 0; i < allChildren.length; i++) {
+				let child = allChildren[i];
+
+				let randomSize = Math.random() * 100;
+
+				let r = Math.floor(Math.random() * 1);
+				let g = Math.floor(Math.random() * 1);
+				let b = Math.floor(Math.random() * 1);
+				let opacity = Math.random();
+
+				child.style.width = randomSize + "px";
+				child.style.height = randomSize + "px";
+				child.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+			}
+		}
+
+		setInterval(changeSizes, 500);
