04ebb4e89261140d2049f202532ef8d20f1820b0
catherinemhuang <chuang05@risd.edu>
2026-04-13 19:57:59 -0400

    added m

--- loop/loop.js
+++ loop/loop.js
@@ -7,16 +7,18 @@ while (i < 100) {
     let randomSize = Math.random() * 200;
 
     let r = Math.floor(Math.random() * 100);
-    let g = Math.floor(Math.random() * 100);
+    let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
     let opacity = Math.random();
 
     let newDiv = `
-				<div style="
-					width: ${randomSize}px;
-					height: ${randomSize}px;
-					background-color: rgba(${r}, ${g}, ${b}, ${opacity});
-				"></div>
+		<div style="
+			width: ${randomSize}px;
+			height: ${randomSize}px;
+			background-color: rgba(${r}, ${g}, ${b}, ${opacity});
+		">
+		m
+	</div>
 			`;
 
     container.innerHTML += newDiv;
