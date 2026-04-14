9f8dded1c44b0494fc8239c7c563b5a215bd22fe
catherinemhuang <chuang05@risd.edu>
2026-04-13 21:49:16 -0400

    added change homepage

--- homepage.js
+++ homepage.js
@@ -1,1 +1,23 @@
 console.log("Hello world!");
+
+function changeStyles() {
+	let allSpans = document.querySelectorAll('span');
+
+	allSpans.forEach((element) => {
+		let r = Math.floor(Math.random() * 255);
+		let g = Math.floor(Math.random() * 255);
+		let b = Math.floor(Math.random() * 255);
+
+		element.style.color = `rgb(${r}, ${g}, ${b})`;
+	});
+
+	let allSize = document.querySelectorAll('size');
+
+	allSize.forEach((element) => {
+		let randomSpacing = Math.random() * 8 - 2;
+		element.style.letterSpacing = randomSpacing + "px";
+	});
+}
+
+setInterval(changeStyles, 300);
+
