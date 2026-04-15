f7c83e961fea59021b159c8b6054544ae2862327
catherinemhuang <chuang05@risd.edu>
2026-04-15 00:47:57 -0400

    changed interaction

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -3,14 +3,13 @@ console.log("This is the interaction site!");
 let block = document.querySelector('.block');
 let numberOfInteractions = 0;
 
-function runAway() {
-	block.style.transform = `translate(${Math.random()*100-50}vw, ${Math.random()*100-50}vh)`;
+function followMouse(e) {
+	block.style.transform = `translate(${e.clientX-75}px, ${e.clientY-75}px)`;
 	numberOfInteractions++;
 	if (numberOfInteractions > 2) {
 		block.innerText = "hahahhaa";
 	}
 	if (numberOfInteractions > 4) {
-		block.innerText = "You can't get me";
-	}
+	block.innerText = "💩";	}
 }
-block.addEventListener('mouseenter', runAway)
+document.addEventListener('mousemove', followMouse);
