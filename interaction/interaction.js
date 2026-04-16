57f05bf0f4a97d76ecc3294f8db4998cc0fd3315
catherinemhuang <chuang05@risd.edu>
2026-04-15 22:41:06 -0400

    update

--- interaction/interaction.js
+++ interaction/interaction.js
@@ -3,13 +3,31 @@ console.log("This is the interaction site!");
 let block = document.querySelector('.block');
 let numberOfInteractions = 0;
 
+let emojiList = [
+	"💩", 
+	"🫚", 
+	"🦪 ", 
+	"🫩", 
+	"🗿", 
+	"🧌"
+];
+let emojiIndex = 0;
+
 function followMouse(e) {
-	block.style.transform = `translate(${e.clientX-75}px, ${e.clientY-75}px)`;
-	numberOfInteractions++;
-	if (numberOfInteractions > 2) {
-		block.innerText = "hahahhaa";
-	}
-	if (numberOfInteractions > 4) {
-	block.innerText = "💩";	}
+  block.style.transform = `translate(${e.clientX - 75}px, ${e.clientY - 75}px)`;
+  numberOfInteractions++;
+
+  if (numberOfInteractions > 1) {
+    block.innerText = "💩";
+  }
 }
+
+function changeEmoji(e) {
+  if (e.code === "Space") {
+    emojiIndex = (emojiIndex + 1) % emojiList.length;
+    block.innerText = emojiList[emojiIndex];
+  }
+}
+
 document.addEventListener('mousemove', followMouse);
+document.addEventListener('keydown', changeEmoji);  
